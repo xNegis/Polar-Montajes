bf80e6b33a5b51be9ff0c53424f96afdf0640880
Alvaro <alvarocortescasado@gmail.com>
2021-01-14 21:43:51 +0100

    Poco a poco

--- src/main/java/org/springframework/samples/petclinic/web/FacturasController.java
+++ src/main/java/org/springframework/samples/petclinic/web/FacturasController.java
@@ -1,18 +1,11 @@
 package org.springframework.samples.petclinic.web;
 
-import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.samples.petclinic.model.Factura;
 import org.springframework.samples.petclinic.service.FacturasService;
 import org.springframework.samples.petclinic.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -50,38 +43,6 @@ public class FacturasController {
 		}
 		return mav;
 
-	}
-
-//	@GetMapping("/crearFactura")
-//	public ModelAndView crearFactura() {
-//		ModelAndView mav = new ModelAndView();
-//		boolean esTrabajador = this.userService.getUserSession().getAuthorities().stream()
-//				.anyMatch(x -> x.getAuthority().equals("gerente"));
-//		if (esTrabajador) {
-//			mav.setViewName("facturas/crearFactura");
-//		} else {
-//			mav.setViewName("welcome");
-//		}
-//		return mav;
-//	}
-
-	@RequestMapping(value = "/crearFactura", method = RequestMethod.GET)
-	public ModelAndView showForm() {
-		ModelAndView mav = new ModelAndView("facturas/crearFactura", "factura", new Factura());
-		return mav;
-	}
-
-	@RequestMapping(value = "/crearFactura", method = RequestMethod.POST)
-	public ModelAndView submit(@Valid @ModelAttribute("factura") Factura factura, BindingResult result) {
-		ModelAndView mav = new ModelAndView();
-		if (!result.hasErrors()) {
-			mav.setViewName("facturas/crearLineasFacturas");
-			this.facturaService.saveFactura(factura);
-			mav.addObject("factura", factura);
-		} else {
-			mav.addObject("errores", result.getAllErrors());
-		}
-		return mav;
 	}
 
 }
