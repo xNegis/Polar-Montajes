7d4473923e0ba05b577e7dcef3fcda43f1166019
Alvaro <alvarocortescasado@gmail.com>
2021-01-14 15:39:29 +0100

    Done

---
+++ src/main/java/org/springframework/samples/petclinic/repository/ClienteRepository.java
@@ -0,0 +1,7 @@
+package org.springframework.samples.petclinic.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ClienteRepository extends CrudRepository<ClienteRepository, Integer> {
+
+}
