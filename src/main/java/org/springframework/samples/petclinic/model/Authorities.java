5fe6058898c5c00b28999e23efdd52f253ab1fab
xNegis <62079678+xNegis@users.noreply.github.com>
2021-01-14 14:31:49 +0100

    Initial commit

---
+++ src/main/java/org/springframework/samples/petclinic/model/Authorities.java
@@ -0,0 +1,27 @@
+package org.springframework.samples.petclinic.model;
+
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "authorities")
+public class Authorities extends BaseEntity{
+	
+	@ManyToOne
+	@JoinColumn(name = "username")
+	User user;
+	
+	@Size(min = 3, max = 50)
+	String authority;
+	
+	
+}
