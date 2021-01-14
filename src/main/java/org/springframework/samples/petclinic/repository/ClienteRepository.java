45b4427b74af326b5dff0ae193c80360fb85cd62
Mariano Manuel Torrado Sánchez <marianomtorrados@gmail.com>
2021-01-14 16:36:33 +0100

    push

--- src/main/java/org/springframework/samples/petclinic/repository/ClienteRepository.java
+++ src/main/java/org/springframework/samples/petclinic/repository/ClienteRepository.java
@@ -1,8 +1,13 @@
 package org.springframework.samples.petclinic.repository;
 
+import java.util.List;
+
+import org.springframework.dao.DataAccessException;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.samples.petclinic.model.Cliente;
+import org.springframework.samples.petclinic.model.Factura;
 
 public interface ClienteRepository extends CrudRepository<Cliente, Integer> {
+	List<Cliente> findAll() throws DataAccessException;
 
 }
--- src/main/java/org/springframework/samples/petclinic/repository/PedidoRepository.java
+++ src/main/java/org/springframework/samples/petclinic/repository/PedidoRepository.java
@@ -1,8 +1,13 @@
 package org.springframework.samples.petclinic.repository;
 
+import java.util.List;
+
+import org.springframework.dao.DataAccessException;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.samples.petclinic.model.Nomina;
 import org.springframework.samples.petclinic.model.Pedido;
 
 public interface PedidoRepository  extends CrudRepository<Pedido, Integer> {
+	List<Pedido> findAll() throws DataAccessException;
 
 }
